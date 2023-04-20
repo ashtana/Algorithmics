d7531b7a82b6d0dcf0cb8e5fce6946f4e42a8cad
ashtana <cslamer@mail.ru>
2023-04-20 23:25:14 +0300

    passion for burgers.c dynamic programming

--- Algorithmic thinking/memoization and dynamic programming/passion for burgers.c
+++ Algorithmic thinking/memoization and dynamic programming/passion for burgers.c
@@ -85,61 +85,96 @@ int max_v(int v1, int v2) {
 
 // Решение с мемоизацией
 
-unsigned long long total_calls;
+// unsigned long long total_calls;
 
-int solve_t(int m, int n, int t, int memo[]) {
-    int first, second;
-    total_calls++;
+// int solve_t(int m, int n, int t, int memo[]) {
+//     int first, second;
+//     total_calls++;
 
-    if (memo[t] != -2)
-        return memo[t];
+//     if (memo[t] != -2)
+//         return memo[t];
 
-    if (t == 0) {
-        memo[t] = 0;
-        return memo[t];
-    }
+//     if (t == 0) {
+//         memo[t] = 0;
+//         return memo[t];
+//     }
     
-    if (t >= m)
-        first = solve_t(m, n, t - m, memo);
-    else
-        first = -1;
-    if (t >= n)
-        second = solve_t(m, n, t - n, memo);
-    else
-        second = -1;
-    if (first == -1 && second == -1) {
-        memo[t] = -1;
-        return memo[t];
-    }
-    else {
-        memo[t] = max_v(first, second) + 1;
-        return memo[t];
-    }
-}
+//     if (t >= m)
+//         first = solve_t(m, n, t - m, memo);
+//     else
+//         first = -1;
+//     if (t >= n)
+//         second = solve_t(m, n, t - n, memo);
+//     else
+//         second = -1;
+//     if (first == -1 && second == -1) {
+//         memo[t] = -1;
+//         return memo[t];
+//     }
+//     else {
+//         memo[t] = max_v(first, second) + 1;
+//         return memo[t];
+//     }
+// }
+
+// #define SIZE 10000
+
+// void solve(int m, int n, int t) {
+//     int result, i;
+//     total_calls = 0;
+//     int memo[SIZE];
+//     for (i = 0; i <= t; i++)
+//         memo[i] = -2;
+//     result = solve_t(m, n, t, memo);
+//     if (result >= 0)
+//         printf("%d\n", result);
+//     else {
+//         i = t - 1;
+//         result = solve_t(m, n, i, memo);
+//         while (result == -1) {
+//             i--;
+//             result = solve_t(m, n, i, memo);
+//         }
+//         printf("%d %d\n", result, t - i);
+//     }
+//     printf("Total calls to solve_t: %llu\n", total_calls);
+// }
+
+// Решение с добавлением динамического программирования
 
 #define SIZE 10000
 
 void solve(int m, int n, int t) {
-    int result, i;
-    total_calls = 0;
-    int memo[SIZE];
-    for (i = 0; i <= t; i++)
-        memo[i] = -2;
-    result = solve_t(m, n, t, memo);
+    int result, i, first, second;
+    int dp[SIZE];
+    dp[0] = 0;
+    for (i = 1; i <= t; i++) {
+        if (i >= m)
+            first = dp[i - m];
+        else
+            first = -1;
+        if (i >= n)
+            second = dp[i - n];
+        else
+            second = -1;
+        if (first == -1 && second == -1)
+            dp[i] = -1;
+        else
+            dp[i] = max_v(first, second) + 1;
+    }
+    result = dp[t];
     if (result >= 0)
         printf("%d\n", result);
     else {
         i = t - 1;
-        result = solve_t(m, n, i, memo);
+        result = dp[i];
         while (result == -1) {
             i--;
-            result = solve_t(m, n, i, memo);
+            result = dp[i];
         }
         printf("%d %d\n", result, t - i);
     }
-    printf("Total calls to solve_t: %llu\n", total_calls);
 }
-
 
 int main(void) {
     int m, n, t;
