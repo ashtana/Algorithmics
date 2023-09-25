707961270521d28e61e108102f9ff43eaa5a1831
ashtana <cslamer@mail.ru>
2023-09-25 21:48:00 +0300

    hockey rivalry dynamic

--- Algorithmic thinking/memoization and dynamic programming/hockey rivalry.c
+++ Algorithmic thinking/memoization and dynamic programming/hockey rivalry.c
@@ -45,7 +45,7 @@
 Время на решение тестового примера — одна секунда.
 */
 
-// Решение 2. Мемоизация
+// Решение 3. Динамическое программирование с оптимизацией использования памяти
 
 #define SIZE 1000
 
@@ -59,32 +59,36 @@ int max_v(int v1, int v2) {
 }
 
 
-int solve(char outcome1[], char outcome2[], int goals1[], int goals2[], int i, int j, int memo[SIZE + 1][SIZE + 1]) {
+int solve(char outcome1[], char outcome2[], int goals1[], int goals2[], int n) {
+   int i, j, k;
    int first, second, third, fourth;
-   if (memo[i][j] != - 1) {
-      return memo[i][j];
+   static int previous[SIZE +1], current[SIZE + 1];
+   for (i = 0; i <= n; i++) {
+      previous[i] = 0;
    }
-   if (i == 0 || j == 0) {
-      memo[i][j] = 0;
-      return memo[i][j];
+   for (i = 1; i <= n; i++) {
+      for (j = 1; j <= n; j++) {
+         if ((outcome1[i] == 'W' && outcome2[j] == 'L' && goals1[i] > goals2[j]) || (outcome1[i] == 'L' && outcome2[j] == 'W' && goals1[i] < goals2[j])) {
+            first = previous[j-1] + goals1[i] + goals2[j];
+         } else {
+            first = 0;
+         }
+         second = previous[j-1];
+         third = previous[j];
+         fourth = current[j-1];
+         current[j] = max_v(first, max_v(second, max_v(third, fourth)));
+      }
+      for (k = 0; k <= SIZE; k++) {
+         previous[k] = current[k];
+      }
    }
-   if ((outcome1[i] == 'W' && outcome2[j] == 'L' && goals1[i] > goals2[j]) || (outcome1[i] == 'L' && outcome2[j] == 'W' && goals1[i] < goals2[j])) {
-      first = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1, memo) + goals1[i] + goals2[j];
-   } else {
-      first = 0;
-   }
-   second = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1, memo);
-   third = solve(outcome1, outcome2, goals1, goals2, i - 1, j, memo);
-   fourth = solve(outcome1, outcome2, goals1, goals2, i, j - 1, memo);
-   memo[i][j] = max_v(first, max_v(second, max_v(third, fourth)));
-   return memo[i][j];
+   return current[n];
 }
 
 int main(void) {
    int i, j, n, result;
    char outcome1[SIZE + 1], outcome2[SIZE + 1];
    int goals1[SIZE + 1], goals2[SIZE + 1];
-   static int memo[SIZE + 1][SIZE + 1];
    scanf("%d ", &n);
    for (i = 1; i <= n; i++) {
       scanf("%c", &outcome1[i]);
@@ -98,13 +102,7 @@ int main(void) {
    for (i = 1; i <= n; i++) {
       scanf("%d ", &goals2[i]);
    }
-   for (i = 0; i <= SIZE; i++) {
-      for (j = 0; j <= SIZE; j++)
-      {
-         memo[i][j] = -1;
-      }
-   }
-   result = solve(outcome1, outcome2, goals1, goals2, n, n, memo);
+   result = solve(outcome1, outcome2, goals1, goals2, n);
    printf("%d\n", result);
    return 0;
 }
